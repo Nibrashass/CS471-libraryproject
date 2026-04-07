d1a5572e921f1fb76644db3819ae93ebf3fcd61f
Nibras <nebrassoon25@gmail.com>
2026-04-07 07:16:43 +0300

    Lab 7 - database queries

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('html5/listing', views.html5_listing, name='html5_listing'),
     path('html5/tables', views.html5_tables, name='html5_tables'),
     path('search', views.search_books),
+    path('simple/query', views.simple_query),
+    path('complex/query', views.complex_query),
 ]
